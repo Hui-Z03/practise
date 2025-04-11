67bf5a85316b7760c765deeb869f6bb08d182b60
Hui <ien0291275@hotmail.com>
2025-04-11 20:47:31 +1000

    optimize time &space complexity

--- running_sum_of_1D_Array.py
+++ running_sum_of_1D_Array.py
@@ -11,8 +11,17 @@ def running_sum(nums):
         result.append(sum)
     return result
 
+# optimize the time and space complexity
+def running_sum_1(nums):
+    for i in range(1, len(nums)):
+        nums[i] += nums[i - 1]
+    return nums
+
 
 
 # example usage
 nums = [1,3,5,6,9,10]
 print(running_sum(nums))
+
+nums_1 = [1,3,5,6,9,10]
+print(running_sum_1(nums_1))
